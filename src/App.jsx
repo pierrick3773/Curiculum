30c758ddc738bfec97387b1492d5989eac2ef04c
Pierrick_Viarde <pviarde@gmail.com>
2024-07-07 19:33:16 +0200

    test loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-
-import './App.css'
+import { useState, useEffect } from 'react';
+import Loader from './assets/component/loader';
+import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <div>
-        <div>
-          <div className="background-container">
-            <div className="stars"></div>
-            <div className="twinkling"></div>
-            <div className="clouds"></div>
-            <div className= "contenu">
-          <img className="logo" src="/unnamed.jpg" alt="Avatar" />
-          </div>
-          </div>
-          
+      {loading && (
+        <div className="loader-container">
+          <Loader />
         </div>
+      )}
+      <div className="background-container">
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
+        {!loading && (
+          <div className="contenu">
+            <img className="logo" src="/unnamed.jpg" alt="Avatar" />
+          </div>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

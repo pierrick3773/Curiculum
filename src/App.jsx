bbf0b0d9eaf5f50fd10a9bdd1141ad65875458db
Pierrick_Viarde <pviarde@gmail.com>
2024-07-07 18:18:48 +0200

    fond et logo centré

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        
+        <div>
+          <div className="background-container">
+            <div className="stars"></div>
+            <div className="twinkling"></div>
+            <div className="clouds"></div>
+            <div className= "contenu">
+          <img className="logo" src="/unnamed.jpg" alt="Avatar" />
+          </div>
+          </div>
+          
+        </div>
       </div>
-      
-       
-      
-      
     </>
   )
 }

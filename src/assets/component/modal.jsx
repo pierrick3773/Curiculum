872c8aeb2a19f02b53a4b9a0558ebd3b563b9bbf
Pierrick_Viarde <pviarde@gmail.com>
2024-08-22 18:31:09 +0200

    contenu dynamique modales

--- src/assets/component/modal.jsx
+++ src/assets/component/modal.jsx
@@ -1,31 +1,44 @@
-import{ useState } from 'react';
+import { useState } from 'react';
 import Modal from 'react-modal';
 import './modal.css';
+import { dataCV } from '../../content_option';
 
 Modal.setAppElement('#root'); // Assurez-vous que l'élément racine de votre application est correctement défini
 
 const MyComponent = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
 
-  const openModal = () => {
+  const openModal = (item) => {
+    setSelectedItem(item);
     setIsOpen(true);
   };
 
   const closeModal = () => {
     setIsOpen(false);
+    setSelectedItem(null);
   };
 
   return (
     <div className="modal-container">
-      <button onClick={openModal}>Open Modal</button>
+      {dataCV.map((item) => (
+        <button key={item.id} onClick={() => openModal(item)}>
+          {item.title}
+        </button>
+      ))}
       <Modal
         isOpen={isOpen}
         onRequestClose={closeModal}
         className="ReactModal__Content"
         contentLabel="Example Modal"
       >
-        <h2>Hello</h2>
-        <button onClick={closeModal}>Close Modal</button>
+        {selectedItem && (
+          <>
+            <h2>{selectedItem.title}</h2>
+            <p>{selectedItem.contenu}</p>
+            <button onClick={closeModal}>Close Modal</button>
+          </>
+        )}
       </Modal>
     </div>
   );

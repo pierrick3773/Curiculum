85bdc50408624b670bcb1462151d280a26847563
Pierrick_Viarde <pviarde@gmail.com>
2024-09-11 16:47:43 +0200

    cards formations

--- src/content_option.jsx
+++ src/content_option.jsx
@@ -1,4 +1,15 @@
+import React from 'react';
 import portfolioImage from '../src/portfolio.png';
+import './assets/component/formationCard.css';
+
+const FormationCard = ({ title, date, className, option, ville }) => (
+  <div className={`formation-card ${className}`}>
+    <h3>{title}</h3>
+    <h4>{option}</h4>
+    <p>{ville}</p>
+    <p>{date}</p>
+  </div>
+);
 
 const dataCV = [
   {
@@ -15,7 +26,7 @@ const dataCV = [
     id: 3,
     title: "À propos de moi",
     contenu: (
-      <p>
+      <p className="description">
         Je suis un Développeur Web Front-End junior passionné, spécialisé dans la création d'interfaces utilisateur conviviales et réactives. J'ai de bonnes connaissances dans les technologies front-end telles que HTML, CSS, JavaScript, avec une préférence pour React.
         <br /><br />
         Mon sens aiguisé du design me permet de transformer les maquettes en interfaces web fonctionnelles et visuellement attrayantes. Je suis également reconnu pour ma capacité à résoudre rapidement les problèmes de code, à optimiser le rendu des pages et à garantir une expérience utilisateur fluide.
@@ -35,8 +46,33 @@ const dataCV = [
   },
   {
     id: 5,
-    title: "Formation",
-    contenu: "Projet d'étude réalisé avec HTML et Sass. Réalisé pour travailler le principe d'animation et l'interactivité avec les utilisateurs.",
+    title: "Formations",
+    contenu: (
+      <div className="formation-cards">
+        <div className="small-cards-container">
+          <FormationCard
+            title="Bac ES"
+            ville="Lycée Monge Chambery"
+            date="2007"
+            className="formation-card-small"
+          />
+          <FormationCard
+            title="DUT carrières sociales "
+            option="Parcours animation sociale et socio-culturelle"
+            ville="IUT Tours"
+            date="2009"
+            className="formation-card-small"
+          />
+        </div>
+        <FormationCard
+          title="Certification Développement Intégrateur Web"
+          option="Diplôme de niveau 5 (bac +2)"
+          ville="OpenClassRooms"
+          date="2024"
+          className="formation-card-large"
+        />
+      </div>
+    ),
   },
 ];
 
